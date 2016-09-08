656096453edd7fe511971c27fcedebac60abfde0
Mirim An <mimicat@Mirims-MacBook-Air.local>
2016-09-08 10:34:21 -0400

    done

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -11,9 +11,39 @@ import UIKit
 class ViewController: UIViewController {
     // TODO: Create IB outlets
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    @IBAction func four(sender: AnyObject) {
+        label1.text = "♣️"
+        label2.text = "4"
+        label3.text = "♣️"
+    }
 
+    @IBAction func three(sender: AnyObject) {
+        label1.text = "♠️"
+        label2.text = "3"
+        label3.text = "♠️"
+        
+    }
+    
+    @IBAction func eight(sender: AnyObject) {
+        label1.text = "♦️"
+        label2.text = "8"
+        label3.text = "♦️"
+    }
+   
+    @IBAction func ten(sender: AnyObject) {
+        label1.text = "♥️"
+        label2.text = "10"
+        label3.text = "♥️"
+    }
+    
     // TODO: IB actions and code to update UI
 }
